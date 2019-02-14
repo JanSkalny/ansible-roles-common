dc00edfe644c934a1f8f1316571a3250cc77f3dc
Martin Kalcok <martin.kalcok@gmail.com>
2019-02-14 13:13:08 +0100

    Improve performance of mysqldump

--- backup-client/templates/make_mysql_backup.sh
+++ backup-client/templates/make_mysql_backup.sh
@@ -3,13 +3,19 @@
 # Simple mysql bacula pre-backup script
 # Last edit: 2016-10-26
 
-DIR="/var/backups/mysql/"
+DIR={{ backup_sql_dump_dir }}
 MYSQLARGS="-u {{ backup_sql_user }} -p{{ backup_sql_password }}"
 
 mkdir -p $DIR
-DATABASES=`mysql $MYSQLARGS -B -e "SHOW DATABASES;" | grep -v '^\(performance_schema\|sys\|information_schema\|Database\)$' | xargs -L 1 -I% echo -n "% "`
 
 for db in $DATABASES; do
-	f="$DIR/$db.sql"
-	mysqldump $MYSQLARGS $db > $f
+    ENGINE=$(mysql $MYSQLARGS --skip-column-names -e "SHOW TABLE STATUS FROM $db;" | grep -v InnoDB | wc -l)
+    if [ $ENGINE -eq 0 ]; then
+        DUMPARGS="--single-transaction"
+    else
+        DUMPARGS=""
+    fi
+    f="$DIR/$db.sql"
+    mysqldump $MYSQLARGS $DUMPARGS $db > $f
+
 done
