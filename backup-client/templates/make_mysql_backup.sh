bbf6fb5581ba92cff64c49c9e9a95769fba2579f
Martin Kalcok <martin.kalcok@gmail.com>
2019-02-26 14:08:59 +0100

    fix mistakenly removed line from script

--- backup-client/templates/make_mysql_backup.sh
+++ backup-client/templates/make_mysql_backup.sh
@@ -8,6 +8,8 @@ MYSQLARGS="-u {{ backup_sql_user }} -p{{ backup_sql_password }}"
 
 mkdir -p $DIR
 
+DATABASES=`mysql $MYSQLARGS -B -e "SHOW DATABASES;" | grep -v '^\(performance_schema\|sys\|information_schema\|Database\)$' | xargs -L 1 -I% echo -n "% "`
+
 for db in $DATABASES; do
     ENGINE=$(mysql $MYSQLARGS --skip-column-names -e "SHOW TABLE STATUS FROM $db;" | grep -v InnoDB | wc -l)
     if [ $ENGINE -eq 0 ]; then
