ad4ece7b171a75b320f1d2e248eac34d04fe3cdd
Jan Skalny <jan@skalny.sk>
2019-04-03 11:48:05 +0200

    multipath: less agressive refresh

--- multipath/files/multipath-refresh.sh
+++ multipath/files/multipath-refresh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for HOST in $(ls -1d /sys/class/fc_host/*); do echo "1" > ${HOST}/issue_lip; done
+#for HOST in $(ls -1d /sys/class/fc_host/*); do echo "1" > ${HOST}/issue_lip; done
 for HOST in $(ls -1d /sys/class/scsi_host/*); do echo "- - -" > ${HOST}/scan ; done
 
 multipath > /dev/null
