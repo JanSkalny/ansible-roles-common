bbfcfdb9af77ca822dad6fe334f24aaea5a9b08d
Jan Skalny <jan@skalny.sk>
2019-11-11 19:23:04 +0100

    suricata

---
+++ suricata/files/sensor-nic.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+fail() {
+	echo "$*" 1>&2
+	exit 1
+}
+
+[ $# -ne 2 ] && fail "missing arguments"
+
+NIC=$1
+OP=$2
+
+case $OP in
+up)
+	ip link set $NIC promisc on arp off up >/dev/null 2>&1
+	echo 1 > /proc/sys/net/ipv6/conf/$NIC/disable_ipv6
+	#ip link set dev $NIC mtu 9000
+
+	ethtool --pause $NIC autoneg off rx off tx off >/dev/null 2>&1
+	for i in rx tx autoneg tso ufo gso gro lro txnocachecopy rxhash ntuple sg txvlan rxvlan; do
+		ethtool -K $NIC $i off >/dev/null 2>&1
+	done
+	;;
+
+down)
+	ifconfig $NIC down 2>&1
+	;;
+*)
+	fail "invalid option"
+	:;
+esac
