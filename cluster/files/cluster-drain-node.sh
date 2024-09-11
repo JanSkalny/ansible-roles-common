83769f8628fc01b6053c813dfd8a7cbe20f4ce7f
Jan Skalny <jan@skalny.sk>
2024-09-11 15:16:49 +0200

    cluster: better cluster-drain script

--- cluster/files/cluster-drain-node.sh
+++ cluster/files/cluster-drain-node.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# tunables
+MIN_OBSERVE_TIME=10
+MAX_OBSERVE_TIME=300
+MAX_MIGRATE_TIME=300
+AUTO_MIGRATE_TIME=5
+WAIT_AFTER_MIGRATION=10
+
 warn() {
   echo "$*" 1>&2
 }
@@ -9,13 +16,69 @@ fail() {
   exit 1
 }
 
-# list of all cluster nodes except our node
-NODES=$( crm status | grep -oP 'Online: \[\K[^\]]+' )
-HOSTNAME=$(hostname -s)
-NODES=(${NODES[@]/$HOSTNAME})
+check_status() {
+  CNT=$( crm status | grep -i "failed" | wc -l | awk '{print $1}' )
+  [ $CNT -ne 0 ] && echo -n "f" && return 1
+
+  CNT=$( crm status | grep "Migrating" | wc -l | awk '{print $1}' )
+  [ $CNT -ne 0 ] && echo -n "m" && return 1
+
+  CNT=$( crm status | grep "Monitoring" | wc -l | awk '{print $1}' )
+  [ $CNT -ne 0 ] && echo -n "o" && return 1
+
+  return 0
+}
+
+IGNORE_HOST=$( hostname -s )
+ASK_FOR_CONFIRMATION=true
+SIMULATE=false
+
+# parse arguments
+while getopts ":as" opt; do
+  case ${opt} in
+    a) ASK_FOR_CONFIRMATION=false ;;
+    s) SIMULATE=true ;;
+    \?) fail "Invalid option: -$OPTARG" ;;
+  esac
+done
+shift $((OPTIND -1))
+
+# remainder is ignore host (defaults to hostname -s)
+if [[ ! -z "$1" ]]; then
+    IGNORE_HOST="$1"
+fi
+
+# get list of all cluster ndoes except ariber and IGNORE_HOST provided
+NODES=($( crm status | grep -oP 'Online: \[\K[^\]]+' | tr ', ' '\n' | grep -v '^$' | grep -v arbiter | grep -v "$IGNORE_HOST" ))
 NODE_INDEX=0
 
+# make sure we have some target nodes
+[ ${#NODES[@]} -eq 0 ] && fail "No valid migration targets!"
+echo "Migration targets are ${NODES[@]}"
+
 for VM in $( virsh list | grep running | awk '{print $2}'); do
+  # observe cluster for at least 10 seconds and make sure everything is ok
+  echo -n "Checking cluster status..."
+  FAILS=$MIN_OBSERVE_TIME
+  ATTEMPTS=0
+  while [[ $FAILS -gt 0 ]]; do
+    ((ATTEMPTS++))
+    ((FAILS--))
+
+    sleep 1 
+    check_status
+    if [[ $? -eq 1 ]]; then
+      # failure resets count-down to 10
+      FAILS=$MIN_OBSERVE_TIME
+    else
+      echo -n "."
+    fi
+
+    # terminate after 300 seconds
+    [[ $ATTEMPTS -ge $MAX_OBSERVE_TIME ]] && fail " UNCLEAN!"
+  done
+  echo " OK"
+
   # get VM name from config xml
   UUID=$(virsh dumpxml "$VM" | xmllint --xpath 'string(/domain/uuid/text())' -)
   [ -z "$UUID" ] && fail "$VM does not have uuid?"
@@ -31,29 +94,46 @@ for VM in $( virsh list | grep running | awk '{print $2}'); do
   # round-robin all cluster nodes when migrating
   NEXT_NODE="${NODES[NODE_INDEX++ % ${#NODES[@]}]}"
 
-  # confirm migration
-  echo "Migrate $NAME to $NEXT_NODE?"
-  read -p "Press Enter to continue..."
+  if [[ "$ASK_FOR_CONFIRMATION" == true ]]; then
+    # confirm migration
+    echo "Migrate $NAME to $NEXT_NODE?"
+    read -p "Press Enter to continue..."
+  else
+    # wait for 5 seconds and then continue
+    echo -n "Will migrate $NAME to $NEXT_NODE in"
+    for i in $(seq "$AUTO_MIGRATE_TIME" -1 1); do
+      echo -n " $i"
+      sleep 1
+    done
+    echo ""
+  fi
 
-  # request migration
-  crm res move "${VM}_vm" $NEXT_NODE > /dev/null
-
-  # observe migration process
-  for I in $( seq 1 300 ); do
+  if [[ "$SIMULATE" == true ]]; then
+    # simulate migration
+    echo "simulated crm res move ${VM}_vm $NEXT_NODE"
     sleep 1
-    STATUS=$( virsh list | grep $VM | awk '{print $3}')
-    virsh list | grep $VM > /dev/null
+  else
+    # request migration
+    crm res move "${VM}_vm" $NEXT_NODE > /dev/null
 
-    # stop waiting
-    if [ $? -eq 1 ]; then
-      echo " migrated!"
-      echo ""
-      break
-    fi
-    echo -n "${STATUS:0:1}"
-  done
+    # observe migration process
+    for I in $( seq 1 $MAX_MIGRATE_TIME ); do
+      sleep 1
+      STATUS=$( virsh list | grep $VM | awk '{print $3}')
+      virsh list | grep $VM > /dev/null
 
-  # if still running, migration failed
-  virsh list | grep $UUID > /dev/null
-  [ $? -eq 1 ] || echo " failed!"done
+      # stop waiting
+      if [ $? -eq 1 ]; then
+        echo " migrated!"
+        echo ""
+        sleep $WAIT_AFTER_MIGRATION
+        break
+      fi
+      echo -n "${STATUS:0:1}"
+    done
+
+    # if still running, migration failed
+    virsh list | grep $UUID > /dev/null
+    [ $? -eq 1 ] || echo " failed!"done
+  fi
 done
