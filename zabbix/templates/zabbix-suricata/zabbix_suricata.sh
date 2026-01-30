233ddd942f00e5471241f91411a83a067da0a413
t14 <wanderer@dotya.ml>
2026-01-30 17:12:02 +0100

    zabbix-suricata: monitor restart count

--- zabbix/templates/zabbix-suricata/zabbix_suricata.sh
+++ zabbix/templates/zabbix-suricata/zabbix_suricata.sh
@@ -6,7 +6,7 @@ fail() {
 }
 
 usage() {
-    fail "usage: $0 (discover-apps|get KEY|json)"
+    fail "usage: $0 (discover-apps|get KEY|json|restart-counter)"
     exit 1
 }
 
@@ -41,6 +41,17 @@ json)
     [ $# -ne 1 ] && usage
     [ -f "$STATS" ] || fail "missing stats.last file"
     jq -Rn '[inputs | split("|") | {(.[0]): (.[2]|tonumber)}] | add ' "$STATS"
+    ;;
+
+restart-counter)
+    [ $# -ne 1 ] && usage
+    # TODO alternatively use `systemctl show service -p NRestarts`
+    J="$(journalctl -xeu suricata --since=-2h --grep 'restart counter is at')"
+    if [[ -z "$J" ]]; then
+        echo 0
+    else
+        echo "$J" | awk '/restart counter is at/{print $NF}' | tail -n 1 | sed -e 's/\.//g'
+    fi
     ;;
 
 *)
